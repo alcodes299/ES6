94f09f165bc63772831e6afa04354216abf920b3
alcodes299 <alskis299@gmail.com>
2018-03-17 07:30:03 -0400

    ground and sky!

---
+++ marioJS/public/js/loaders.js
@@ -0,0 +1,10 @@
+function loadImage(url){
+  return new Promise(resolve =>{
+    const image = new Image()
+    image.addEventListener('load', () => {
+      resolve(image)
+    })
+    image.src = url
+  })
+}
+export default loadImage
--- marioJS/public/js/main.js
+++ marioJS/public/js/main.js
@@ -1,44 +1,6 @@
-function loadImage(url){
-  return new Promise(resolve =>{
-    const image = new Image()
-    image.addEventListener('load', () => {
-      resolve(image)
-    })
-    image.src = url
-  })
-}
+import SpriteSheet from './spritesheet.js'
+import loadImage from './loaders.js'
 
-class SpriteSheet {
-  constructor(image, width, height) {
-    this.image = image
-    this.width = width
-    this.height = height
-    this.tiles = new Map()
-  }
-  define(name,x,y){
-    const buffer = document.createElement('canvas')
-    buffer.width = this.width
-    buffer.height = this.height
-    buffer
-      .getContext('2d')
-      .drawImage(
-        this.image,
-        x * this.width,
-        y * this.width,
-        this.width,
-        this.height,
-        0,
-        0,
-        this.width,
-        this.height)
-      this.tiles.set(name, buffer)
-  }
-  draw(name, context, x, y){
-    const buffer = this.tiles.get(name)
-    context.drawImage(buffer, x, y)
-  }
-
-}
 
 const canvas = document.getElementById('screen')
 const context = canvas.getContext('2d')
@@ -46,10 +8,18 @@ context.fillRect(0,0,50,50)
 
 loadImage('/img/tiles.png')
 .then(image => {
-  const sprites = new SpriteSheet(image, 16, 16)
-  sprites.define('ground',0, 0)
-  sprites.draw('ground',context, 45, 62)
+  const sprite = new SpriteSheet(image, 16, 16)
+  sprite.define('ground',0, 0)
+  sprite.define('sky', 3,23)
 
-
-
+  for(let x = 0; x < 25;++x){
+    for(let y = 0;y < 14;y++){
+      sprite.drawTile('sky',context, x, y)
+    }
+  }
+  for(let x = 0; x < 25;++x){
+    for(let y = 12;y < 14;y++){
+      sprite.drawTile('ground',context, x, y)
+    }
+  }
 })

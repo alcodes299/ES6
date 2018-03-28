75a24c0a7da72641e8b3f5248eebe0d7d546e181
alcodes299 <alskis299@gmail.com>
2018-03-28 15:53:59 -0400

    digi cube 1

--- new_game/sketch.js
+++ new_game/sketch.js
@@ -1,25 +1,56 @@
 let angle = 0
 let w = 24
+let ma
+let maxD
+let cola = 'lightblue'
 function setup(){
-  createCanvas(400, 400, WEBGL)
+  createCanvas(800, 800 , WEBGL)
+  ma = atan(1 / sqrt(2))
+  maxD = dist(0, 0, 200, 200)
+  frameRate(100)
+
+  countUp = 0
+  colorTime = 20
 }
-function draw(){
-  background(175);
-  // translate(width / 2, height / 2)
-  rectMode(CENTER)
+function draw()
+{
+  background(cola)
+  ortho(-1000, 1000, 1000, -1000, 0, 1000);
+  rotateX(QUARTER_PI)
+  rotateY(ma)
 
-  let offset = 0;
-  for (var i = 0; i < width; i+= 24) {
-    let a = angle + offset
+  countUp += 1
+  if (countUp % colorTime == 0) {
+    var rand1 = Math.floor(Math.random() * 250)
+    var rand2 = Math.floor(Math.random() * 250)
+    var rand3 = Math.floor(Math.random() * 250)
+    var rand4 = Math.floor(Math.random() * 250)
+    var rand5 = Math.floor(Math.random() * 250)
+    var rand6 = Math.floor(Math.random() * 250)
+    fill(rand3,rand1,rand2)
+    stroke(rand4,rand5,rand6)
+    countUp = 0
+    colorTime += colorTime/2
 
-    let h = map(sin(a), -1, 1, 0, 100)
-    fill(255)
-    rect( i - width / 2 + w / 2, 0, 10 , h)
-    offset += 0.1
+    console.log(colorTime)
   }
+  if (colorTime > 60) {
+    colorTime = 20
+  }
+  for (var z = 0; z < height; z += w) {
+    for (var x = 0; x < width; x += w) {
+    push()
+    let d = dist(x, z, width / 2, height / 2)
+    let offset = map(d, 0 , maxD, -PI, PI)
+    let a = angle + offset
+    let h = map(sin(a), -1, 1, 12, 600)
 
-  angle += 0.05
+    translate(x - width / 2, 0, z )
+    box(w ,h , w)
 
+    pop()
+    }
+  }
+  angle += 0.1
 
 }
-draw()

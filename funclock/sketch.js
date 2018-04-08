517ca8c0aff74dab4641f876b351be9bfb9aa488
alcodes299 <alskis299@gmail.com>
2018-04-08 05:36:28 -0400

    done clock 1

--- funclock/sketch.js
+++ funclock/sketch.js
@@ -5,21 +5,62 @@ function setup(){
 
 function  draw(){
   background(0)
+  translate(200,200)
+  rotate()
+
+
   let hr = hour()
   let mn = minute()
   let sc = second()
-  let hrag = map(sc, 0, 60, 0, 360)
+  let scag = map(sc, 0, 60, 0, 360)
+  let mnag = map(mn,0, 60, 0, 360)
+  let hrag = map(hr, 0, 12, 0, 360)
 
   fill(255)
   noStroke()
-  text(hr+':'+mn+':'+sc, 200 - 20, 200);
+
+
 
   noFill()
-  stroke("white")
+  stroke("lightgreen")
+  //hours
+  arc(0,0, 60,60, 0, hrag)
+  arc(0,0, 70,70, 0, hrag)
+  arc(0,0, 80,80, 0, hrag)
 
-  arc(200,200, 160,160, 0, hrag)
-  arc(200,200, 160,160, 0, hrag)
-  arc(200,200, 160,160, 0, hrag)
+  //minutes
+  stroke("blue")
+  arc(0,0, 140,140, 0, mnag)
+  arc(0,0, 145,145, 0, mnag)
+  arc(0,0, 150,150, 0, mnag)
 
-  
+  //secounds
+  stroke("red")
+  arc(0,0, 200,200, 0, scag)
+  arc(0,0, 204,204, 0, scag)
+  arc(0,0, 208,208, 0, scag)
+
+  push()
+  rotate(scag)
+  strokeWeight(2)
+  stroke("purple")
+  line(0,0,200,0)
+  pop()
+
+  push()
+  strokeWeight(1)
+  stroke('pink')
+  rotate(mnag)
+  line(0,0,200,0)
+  pop()
+
+  push()
+  strokeWeight(1)
+  stroke('yellow')
+  rotate(hrag)
+  line(0,0,300,0)
+  pop()
+  stroke('#75FA6C')
+
+  text(hr+':'+mn+':'+sc,-18,60)
 }
